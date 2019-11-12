8757c6cb8f4c34a54fd678e647a2aeee4770a70e
Nurislam <kalenov.n@gmail.com>
2019-11-12 23:36:16 +0600

    2:09:50

--- iwantcode/js/main.js
+++ iwantcode/js/main.js
@@ -3,6 +3,6 @@ $(
       $('.slider__inner').slick({
 nextArrow:'<button type= "button" class="slick-btn slick-next"></button>',
 prevArrow:'<button type= "button" class="slick-btn slick-prev"></button>',
-
+infinite: false
       });
  });
