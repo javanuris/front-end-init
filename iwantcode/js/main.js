293d009a6ef50cf93d46b209fc1adca4530f3f0e
Nurislam <kalenov.n@gmail.com>
2019-11-13 23:04:17 +0600

    3:52:38

--- iwantcode/js/main.js
+++ iwantcode/js/main.js
@@ -1,8 +1,10 @@
 $(
-  function(){
-      $('.slider__inner').slick({
-nextArrow:'<button type= "button" class="slick-btn slick-next"></button>',
-prevArrow:'<button type= "button" class="slick-btn slick-prev"></button>',
-infinite: false
-      });
- });
+    function() {
+        $('.slider__inner').slick({
+            nextArrow: '<button type= "button" class="slick-btn slick-next"></button>',
+            prevArrow: '<button type= "button" class="slick-btn slick-prev"></button>',
+            infinite: false
+        });
+
+        $('select').styler();
+    });
